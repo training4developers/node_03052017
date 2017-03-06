3a597adf203a5aa47f89b2285f55e93034fab40b
Eric W. Greene <eric@ericwgreene.com>
2017-03-06 11:40:21 -0800

    added code demos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+1. Create several static html file
+2. Use the URL information to load the correct static file
+3. Move the code to read the file from disk to a separate module
+4. EXTRA CREDIT: Manage the asynchronous call to read the file with a promise
+
+
 import http from 'http';
 import fs from 'fs';
 import path from 'path';
